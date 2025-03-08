3b9b1c572a58f1e41a8c664a318fa631a8dbd044
MuhammadAffan-114 <bscs23114@itu.edu.pk>
2025-03-09 01:24:37 +0500

    update hex

--- include/hex.h
+++ include/hex.h
@@ -2,13 +2,15 @@
 #define HEX_H
 
 typedef struct hex {
-	const char* word;
-	const char* translation;
+	char character;
+	char* translation;
 	size_t offset;
 } hex;
 
-void init_hex(hex*, const char*, size_t);
-void set_translation(hex*, const char*);
+void init_hex(hex*, const char, size_t);
+void set_translation(hex*);
+void change_translation(hex*, const char*);
+void change_character(hex*);
 void set_offset(hex*, size_t);
 void delete_hex(hex*);
 
--- src/hex.c
+++ src/hex.c
@@ -1,3 +1,34 @@
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "include/hex.h"
+
+void init_hex(hex* h, const char character, size_t offset) {
+	h->character = character;
+	h->translation = (char*)malloc(3);
+	set_translation(h);
+	h->offset = offset;
+}
+
+void set_translation(hex* h) {
+	sprintf(h->translation, "%02X", h->character);
+}
+
+void change_translation(hex* h, const char* translation) {
+	h->translation = translation;
+}
+
+void change_character(hex* h) {
+	h->character = (char)strtol(h->translation, NULL, 16);
+}
+
+void set_offset(hex* h, size_t offset) {
+	h->offset = offset;
+}
+
+void delete_hex(hex* h) {
+	free(h->translation);
+	h->translation = NULL;
+	h->character = 0;
+	h->offset = 0;
+}
